6c55adf22c25535ab8cfc29336e8ed1be0aae1f2
Shafagh <shafaghrezania7@gmail.com>
2025-10-10 04:08:26 +0330

    Add files via upload

---
+++ Homeworks/Exercise1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Homeworks
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double radius, Mohit, Masahat;
+            Console.Write("Enter circle radius: ");
+            radius = double.Parse(Console.ReadLine());
+
+            Mohit = 2 * Math.PI * radius;
+            Masahat = Math.PI * radius * radius;
+
+            Console.WriteLine("Masahat is {0} and Mohit is {1}", Masahat, Mohit);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Homeworks/Exercise2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius, Fahrenheit;
+            Console.Write("Enter a number: ");
+            celsius = double.Parse(Console.ReadLine());
+
+            Fahrenheit = (celsius * 9.5) + 32;
+            Console.WriteLine("The temperture in fahrenheit is " + Fahrenheit);
+            Console.ReadKey();
+
+        }
+    }
+}
