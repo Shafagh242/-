4c69d2dc76891231edded5a9570569dc09073905
Shafagh <shafaghrezania7@gmail.com>
2025-10-10 17:52:45 +0330

    Add files via upload

---
+++ Homeworks2/Exercise1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Homeworks2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Please enter a number: ");
+            a = Int32.Parse(Console.ReadLine());
+
+            if (a < 10 && a % 2 == 0)
+            {
+                Console.WriteLine("\nTRUE!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homeworks2/Exercise2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Please enter a number: ");
+            a = Int32.Parse(Console.ReadLine());
+
+            if (a % 7 == 0)
+                Console.WriteLine("\nYES!");
+            else
+                Console.WriteLine($"\nResult: {a * 3}");
+            Console.ReadKey();
+               
+
+        }
+    }
+ 
+}
