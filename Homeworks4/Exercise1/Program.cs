2fe7a08985c13f4696b0c7cb527adf2df24fce66
Shafagh <shafaghrezania7@gmail.com>
2025-11-19 17:09:27 +0330

    Add files via upload

---
+++ Homeworks4/Exercise1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Please enter a number: ");
+                numbers[i] = Int32.Parse(Console.ReadLine());
+            }
+            int evenCount = 0;
+            int oddCount = 0;
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                    evenCount++;
+                else
+                    oddCount++;
+            }
+            Console.WriteLine("\nNumber of even numbers: " + evenCount);
+            Console.WriteLine("\nNumber of odd numbers: " + oddCount);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homeworks4/Exercise2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Please enter a number: ");
+
+                numbers[i] = Int32.Parse(Console.ReadLine());
+            }
+            bool found = false;
+            int position = -1;
+            for (int i = 0; i < 5; i++)
+            {
+                if (numbers[i] == 2)
+                {
+                    found = true;
+                    position = i + 1;
+
+                    break;
+                }
+            }
+            Console.WriteLine("\n");
+            if (found)
+                Console.WriteLine("Number 2 found at position: " + position);
+            else
+                Console.WriteLine("Number 2 wasn't found.");
+            Console.ReadKey();
+              
+        }           
+                    
+
+    }
+}
