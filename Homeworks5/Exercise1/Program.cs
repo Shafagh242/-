26c96348ec4465bcdb18658083a671718a3d15af
Shafagh <shafaghrezania7@gmail.com>
2025-11-22 16:37:49 +0330

    Add files via upload

---
+++ Homeworks5/Exercise1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        public static void Mohit(double a, double b, double c)
+        {
+            double m = a + b + c;
+            Console.WriteLine("Mohit = " + m);
+        }
+        public static double Masahat(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return area;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 3 sides of triangle:");
+            Console.Write("a= ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b= ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("c= ");
+            double c = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n********************");
+            Mohit(a, b, c);
+            Console.WriteLine("Masahat = " + Masahat(a, b, c));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homeworks5/Exercise2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static int Multiply(int a, int b)
+        {
+            if (b == 0)
+                return 0;
+            return a + Multiply(a, b - 1);
+        }
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.Write("Enter first number: ");
+            x = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            y = Int32.Parse(Console.ReadLine());
+            int result = Multiply(x, y);
+            Console.WriteLine("\n\nResult= " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
